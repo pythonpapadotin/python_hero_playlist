029a05e836cc0200cc928f4aa3dc422ab75383e8
Mohit Singh Chouhan <mohitchouhan1947@gmail.com>
2026-06-16 04:20:47 +0530

    Add files via upload

---
+++ names_and_storage.py
@@ -0,0 +1,12 @@
+x = 45
+
+y = "mango"
+
+z = 45.67
+
+list_of_fruits = ['apple', 'mango', 'banana', 300]
+
+data = {'employee_id': 375,
+        'employee_name': 'Rohan',
+        'employee_age': 42
+        }
